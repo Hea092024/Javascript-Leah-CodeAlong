f1ad4ea3ef463535de6b9363b6eb4c98b047c2c4
Hea092024 <Hea092024@gmail.com>
2025-01-06 11:37:41 +0100

    kommentert ut første function

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-function greetings (){
+// function greetings (){
     // setTimeout(function () {
 
     //     console.log("hei")}, 10000)
-    
+
 // SetTimeout = gir en ønsket "delay" 
 
-    console.log("hei");
-};
+//     console.log("hei");
+// };
 
-function goodbye (){
-    console.log("hade");
-};
+// function goodbye (){
+//     console.log("hade");
+// };
 
-greetings()
-goodbye()
+// greetings()
+// goodbye()
+
